9a93a351d5d43fada0c486680a9a7846797655d4
Erenox <Erenox@hotmail.fr>
2017-01-17 19:28:51 +0100

    Update deploy_processing.rb

--- bin/ScanityManager/lib/processing/deploy_processing.rb
+++ bin/ScanityManager/lib/processing/deploy_processing.rb
@@ -1,4 +1,4 @@
-# ./lib/processing/server.rb
+# ./lib/processing/deploy_processing.rb
 # processing installation of environment
 # Created by : Erenox the : 02/12/2016
 # Last update : 16/12/2016
@@ -36,10 +36,10 @@ class Deploy
       end
 
       # create group of service administrators
-      System.exec('create the service administrators unix group.', 'sudo addgroup scanity_operators', 'scanity_operators group created successfully.', 'scanity_operators group is existent.')
+      System.exec('create the service administrators unix group.', 'sudo addgroup scanity_operators', 'scanity_operators group created.', 'failed to create scanity_operators group.')
 
       # chown rights to scanity operators and service owners
-      System.exec('set directory owners.', "sudo chown -R #{service_owner}:scanity_operators #{@current_dir}/../../", 'directory owners are set successfully.', 'directory owners assignation failed.')
+      System.exec('set directory owners.', "sudo chown -R #{service_owner}:scanity_operators #{@current_dir}/../../", 'directory owners are set.', 'failed to assign directory owners.')
 
   end
   #</editor-fold>
@@ -47,49 +47,49 @@ class Deploy
   #<editor-fold desc="method : update">
   def self.update
     # update the repository
-    System.exec('update system repository.', 'sudo apt-get update', 'update is done.', 'update process failed.')
+    System.exec('update system repository.', 'sudo apt-get update', 'update is done.', 'failed to update.')
   end
   #</editor-fold>
 
   #<editor-fold desc="method : add_repositories">
   def self.add_repositories
     # install mongodb repository key and create a repository file
-    System.exec('setup mongodb repository key.','sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6','mongodb repository key installed','fail to install mongodb repository key')
-    System.exec('create a repository file for mongodb.','sudo echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list','mongodb repository file installed','fail to install mongodb repository file')
+    System.exec('add mongodb repository key.','sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6','mongodb repository key installed.','failed to install mongodb repository key.')
+    System.exec('create source file for mongodb.','sudo echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list','mongodb repository file installed.','fail to install mongodb repository source file.')
   end
   #</editor-fold>
 
   #<editor-fold desc="method : npm_install_general_module(general_module)">
   def self.npm_install_general_module(general_module)
     # install a required general_module via npm
-    System.exec("npm install #{general_module} as general module.","sudo npm install #{general_module} -g","#{general_module} module successfully installed." ,"#{general_module} module installation failed.")
+    System.exec("npm install : #{general_module} as general module.","sudo npm install #{general_module} -g","module : #{general_module} installed." ,"failed to install : #{general_module} general module.")
   end
   #</editor-fold>
 
   #<editor-fold desc="method : server_package_installer(package)">
   def self.server_package_installer(package)
     # install a required server package via apt
-    System.exec("install #{package} service.","sudo apt-get --yes --allow-unauthenticated install #{package}","#{package} successfully installed." ,"#{package} installation failed.")
+    System.exec("apt install : #{package} service.","sudo apt-get --yes --allow-unauthenticated install #{package}","package : #{package} installed." ,"failed to install : #{package} package.")
   end
   #</editor-fold>
 
   #<editor-fold desc="method : audit_package_installer(package)">
   def self.audit_package_installer(package)
     # install a required backups tool package via apt
-    System.exec("install #{package} package.","sudo apt-get --yes install #{package}","#{package} successfully installed." ,"#{package} installation failed.")
+    System.exec("apt install : #{package} audit package.","sudo apt-get --yes install #{package}","audit package : #{package} installed." ,"failed to install : #{package} audit package.")
   end
   #</editor-fold>
 
   #<editor-fold desc="method : custom_package_installer(package)">
   def self.custom_package_installer(package)
     # install custom private required backups packages via local deb
-    System.exec("install private #{package} package.","sudo apt --yes install #{@current_dir}/custom_packages/#{package}.deb","#{package} successfully installed." ,"#{package} installation failed.")
+    System.exec("depack : #{package} onboard package.","sudo apt --yes install #{@current_dir}/custom_packages/#{package}.deb"," onboard package : #{package} installed." ,"failed to install : #{package} onboard package.")
   end
   #</editor-fold>
 
   #<editor-fold desc="method : bundle_installer(name)">
   def self.bundle_installer(name)
-    System.exec("install gems dependencies for #{name} package.","sudo bundle install --gemfile '/usr/share/#{name}/Gemfile'","#{name} dependencies successfully installed." ,"#{name}  dependencies installation failed.")
+    System.exec("install gems dependencies for : #{name} package.","sudo bundle install --gemfile '/usr/share/#{name}/Gemfile'","gems dependencies for : #{name} installed." ," failed to install : #{name} gems dependencies.")
   end
   #</editor-fold>
 
@@ -118,20 +118,20 @@ class Deploy
     # add missing repositories
     add_repositories()
 
-    # update the repository
+    # update the OS repository
     update()
 
-    # install a required server package via apt
+    # install all required service package via apt
     apt_core.each do |package|
       server_package_installer(package)
     end
 
-    # install a required audit tool package via apt
+    # install all required audit tools package via apt
     apt_audit.each do |package|
       audit_package_installer(package)
     end
 
-    # install custom audit tooy packages via local deb
+    # install all audit tools packages via onboard deb
     apt_custom.each do |package|
       custom_package_installer(package)
     end
@@ -141,7 +141,7 @@ class Deploy
       npm_install_general_module(general_module)
     end
 
-    # install gems dependencies
+    # install some missing gems dependencies
     bundle.each do |name|
       bundle_installer(name)
     end
