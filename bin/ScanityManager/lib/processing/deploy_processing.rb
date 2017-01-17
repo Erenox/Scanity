4cb4b9165a5dabff194a3b8c2b3620472ac4f97d
Erenox <Erenox@hotmail.fr>
2017-01-17 22:04:47 +0100

    Update deploy_processing.rb

--- bin/ScanityManager/lib/processing/deploy_processing.rb
+++ bin/ScanityManager/lib/processing/deploy_processing.rb
@@ -52,11 +52,13 @@ class Deploy
   #</editor-fold>
 
   #<editor-fold desc="method : add_repositories">
+=begin
   def self.add_repositories
     # install mongodb repository key and create a repository file
     System.exec('add mongodb repository key.','sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6','mongodb repository key installed.','failed to install mongodb repository key.')
     System.exec('create source file for mongodb.','sudo echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list','mongodb repository file installed.','fail to install mongodb repository source file.')
   end
+=end
   #</editor-fold>
 
   #<editor-fold desc="method : npm_install_general_module(general_module)">
@@ -116,7 +118,7 @@ class Deploy
     set_permissions()
 
     # add missing repositories
-    add_repositories()
+    # add_repositories() // disabled for now
 
     # update the OS repository
     update()
